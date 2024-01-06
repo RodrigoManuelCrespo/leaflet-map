21240a640f194b1e42dc84edf996d1426a6a0dc4
Rodrigo Crespo <cresporodrigomanuel@gmail.com>
2024-01-06 17:04:41 -0300

    Add query params

--- src/app/component/map.js
+++ src/app/component/map.js
@@ -1,36 +1,45 @@
 'use client'
 
 import React, { useEffect, useRef, useState } from 'react';
-import { Map, marker, tileLayer } from 'leaflet';
+import L, { Map, marker, tileLayer, } from 'leaflet';
 import 'leaflet/dist/leaflet.css'
+import { useSearchParams } from 'next/navigation'
 
 function MapComponent() {
+    const searchParams = useSearchParams()
+    const lng = searchParams.get('lng')
+    const lat = searchParams.get('lat')
+
+    var greenIcon = L.icon({
+        iconUrl: 'placeholder.png',
+        iconSize: 50,
+    });
+
     const [map, setMap] = useState();
     const mapInit = useRef(false);
     const initMap = () => {
         map && tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            maxZoom: 50,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         }).addTo(map);
     }
-    const addMarker = () => { // <===============
-        map && marker([-33.8678, 151.21]).addTo(map)
+    const addMarker = () => {
+        map && marker([lat, lng], { icon: greenIcon }).addTo(map)
     }
 
     useEffect(() => {
         if (!mapInit.current) {
-            // Damos por inicializado el mapa
             mapInit.current = true;
-            // Asignamos el contenedor del mapa
             setMap(
                 new Map('map', {
-                    center: [-33.8678, 151.21], // Centramos en Sidney
+                    center: [lat, lng],
                     zoom: 15,
-                }).setView([-33.8678, 151.21]) // Sidney
+                }).setView([lat, lng])
             )
         }
         if (map) {
             initMap();
+            addMarker();
         }
     }, [mapInit, map])
     return (
