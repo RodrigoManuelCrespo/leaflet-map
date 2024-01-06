450fb59a268f4ba9d4d3c58e0d051eb5ed6ff2c1
Rodrigo Crespo <cresporodrigomanuel@gmail.com>
2024-01-06 01:29:35 -0300

    Test pipeline

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,27 +1,27 @@
-'use client'
+// 'use client'
 
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'
-import { icon } from 'leaflet';
+// import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+// import 'leaflet/dist/leaflet.css'
+// import { icon } from 'leaflet';
 
-const ICON = icon({
-    iconUrl: './placeholder.png',
-    iconSize: [40, 40],
-})
+// const ICON = icon({
+//     iconUrl: './placeholder.png',
+//     iconSize: [40, 40],
+// })
 
-const Mapa = () => {
-    return (
-        <div style={{ width: "100%", height: "100%" }}>
-            <MapContainer style={{ height: '100vh', width: '100wh' }}
-                center={[51.505, -0.09]} zoom={50} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker icon={ICON} position={[51.505, -0.09]} />
-            </MapContainer>
-        </div>
-    );
-};
+// const Mapa = () => {
+//     return (
+//         <div style={{ width: "100%", height: "100%" }}>
+//             <MapContainer style={{ height: '100vh', width: '100wh' }}
+//                 center={[51.505, -0.09]} zoom={50} scrollWheelZoom={false}>
+//                 <TileLayer
+//                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//                 />
+//                 <Marker icon={ICON} position={[51.505, -0.09]} />
+//             </MapContainer>
+//         </div>
+//     );
+// };
 
-export default Mapa;
+// export default Mapa;
